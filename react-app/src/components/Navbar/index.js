7eb6d559ddb5e9c0b7720a876cf4b87f9dbfcb18
justin chau <jchau623@gmail.com>
2022-08-22 17:17:52 -0400

    navbar css

--- react-app/src/components/Navbar/index.js
+++ react-app/src/components/Navbar/index.js
@@ -7,30 +7,30 @@ import flickrLogo from '../../assets/flickrLogo.svg'
 import ProfileButton from './ProfileButton';
 
 const NavBar = () => {
-  const sessionUser = useSelector(state => state?.session?.user);
-  return (
-    <nav className="navbar">
-        <div className="navbar-left">
-            <NavLink
-                to="/"
-                className="nav-items"
-            >
-              <div className='nav-home-btn'>
-                <img src={flickrLogo} className="flickr-dots" alt=""/>
-                <h1 className="glimmr">glimmr</h1>
-              </div>
-            </NavLink>
-            {sessionUser && (
-                <>
-                    <div className="nav-items">
-                        <NavLink
-                            to="/photostream"
-                            exact={true}
-                            activeClassName="active"
-                        >
-                            You
-                        </NavLink>
+    const sessionUser = useSelector(state => state?.session?.user);
+    return (
+        <nav className="navbar">
+            <div className="navbar-left">
+                <NavLink
+                    to="/"
+                    className="nav-items"
+                >
+                    <div className='nav-home-btn'>
+                        <img src={flickrLogo} className="flickr-dots" alt="" />
+                        <h1 className="glimmr">glimmr</h1>
                     </div>
+                </NavLink>
+                {sessionUser && (
+                    <>
+                        <div className="nav-items">
+                            <NavLink
+                                to="/photostream"
+                                exact={true}
+                                activeClassName="active"
+                            >
+                                You
+                            </NavLink>
+                        </div>
                         <div className="nav-items">
                             <NavLink
                                 to="/explore"
@@ -40,28 +40,36 @@ const NavBar = () => {
                                 Explore
                             </NavLink>
                         </div>
-                </>
-            )}
-        </div>
-
-        <div className="navbar-center">
-            <div className="nav-items">
-                <a href="https://github.com/jchau-623" target="_blank" rel="noreferrer">
-                    <span className="github-icon">
-                        <i className="fab fa-github"></i>
-                    </span>
-                </a>
+                    </>
+                )}
             </div>
-            <div className="nav-items">
-                <a
-                    href="https://www.linkedin.com/in/justin-chau-1123a9142/"
-                    target="_blank"
-                    rel="noreferrer"
-                >
-                    <span className="linkedin-icon">
-                        <i className="fab fa-linkedin"></i>
-                    </span>
-                </a>
+
+            <div className="navbar-center">
+                <div className="nav-items">
+                    <a href="https://github.com/jchau-623" target="_blank" rel="noreferrer">
+                        <span className="github-icon">
+                            <i className="fab fa-github"></i>
+                        </span>
+                    </a>
+                </div>
+                <div className="nav-items">
+                    <a
+                        href="https://www.linkedin.com/in/justin-chau-1123a9142/"
+                        target="_blank"
+                        rel="noreferrer"
+                    >
+                        <span className="linkedin-icon">
+                            <i className="fab fa-linkedin"></i>
+                        </span>
+                    </a>
+                </div>
+                <div className="nav-items">
+                    <a href="https://angel.co/u/justin-chau-3" target="_blank" rel="noreferrer">
+                        <span className="linkedin-icon">
+                            <i className="fab fa-angellist"></i>
+                        </span>
+                    </a>
+                </div>
             </div>
             <div className="nav-items">
                 <a href="https://jchau-623.github.io/" target="_blank" rel="noreferrer">
@@ -70,47 +78,39 @@ const NavBar = () => {
                     </span>
                 </a>
             </div>
-            <div className="nav-items">
-                <a href="https://https://angel.co/u/justin-chau-3" target="_blank" rel="noreferrer">
-                    <span className="linkedin-icon">
-                        <i className="fab fa-angellist"></i>
-                    </span>
-                </a>
+
+            {sessionUser && <div></div>}
+
+            <div className="navbar-right">
+                {sessionUser ? (
+                    <>
+                        <div className="nav-items">
+                            <NavLink to="/photos/new">
+                                <span className="upload-button">
+                                    <i className="fas fa-cloud-upload-alt"></i>
+                                </span>
+                            </NavLink>
+                        </div>
+                        <div>
+                            <ProfileButton />
+                        </div>
+                    </>
+                ) : (
+                    <>
+                        <div className="nav-items">
+                            <NavLink
+                                to="/sign-up"
+                                exact={true}
+                                activeClassName="active"
+                            >
+                                Sign Up
+                            </NavLink>
+                        </div>
+                    </>
+                )}
             </div>
-        </div>
-
-        {sessionUser && <div></div>}
-
-        <div className="navbar-right">
-            {sessionUser ? (
-                <>
-                    <div className="nav-items">
-                        <NavLink to="/photos/new">
-                            <span className="upload-button">
-                                <i className="fas fa-cloud-upload-alt"></i>
-                            </span>
-                        </NavLink>
-                    </div>
-                    <div>
-                      <ProfileButton />
-                    </div>
-                </>
-            ) : (
-                <>
-                    <div className="nav-items">
-                        <NavLink
-                            to="/sign-up"
-                            exact={true}
-                            activeClassName="active"
-                        >
-                            Sign Up
-                        </NavLink>
-                    </div>
-                </>
-            )}
-        </div>
-    </nav>
-);
+        </nav>
+    );
 };
 
 export default NavBar;
