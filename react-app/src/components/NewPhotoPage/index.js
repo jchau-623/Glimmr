90ae97d4268fcd0b5657860c77a2d7e91e455f5a
justin chau <jchau623@gmail.com>
2022-08-26 12:16:54 -0400

    new photo page

--- react-app/src/components/NewPhotoPage/index.js
+++ react-app/src/components/NewPhotoPage/index.js
@@ -4,3 +4,5 @@ import { useState, useEffect } from "react";
 import { Redirect, useHistory } from "react-router-dom";
 import { postPhoto } from "../../store/photo";
 import { useDispatch, useSelector } from "react-redux";
+
+return
