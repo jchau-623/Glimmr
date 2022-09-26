31aec56f553d91a1222efdd0d4d47ee5db76bdfa
justin chau <jchau623@gmail.com>
2022-09-26 15:04:09 -0400

    photostream page

--- react-app/src/components/PhotostreamPage/index.js
+++ react-app/src/components/PhotostreamPage/index.js
@@ -1,4 +1,14 @@
+import { useDispatch, useSelector } from "react-redux";
+
 const PhotostreamPage = () => {
+    const allPhotosObj = useSelector(store => store?.photo?.photos);
+    let sessionUserPhotos;
+    if (allPhotosObj) {
+        const allPhotosArr = Object?.values(allPhotosObj);
+        sessionUserPhotos = allPhotosArr?.filter(
+            photo => photo?.user_id === props?.sessionUser?.id
+        );
+    }
     return (
         <h1>Photostream Page</h1>
     )
